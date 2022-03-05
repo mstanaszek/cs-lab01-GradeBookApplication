9fc42fc72f2e715610dc41a7f0f9b4f65f44e2d2
Marcin <mstanaszek@onet.eu>
2022-03-05 13:15:57 +0100

    Add Type property

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,6 +14,7 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
+        public GradeBookType Type { get; set; }
         public BaseGradeBook(string name)
         {
             Name = name;
@@ -266,5 +267,6 @@ namespace GradeBook.GradeBooks
             
             return JsonConvert.DeserializeObject(json, gradebook);
         }
+
     }
 }
